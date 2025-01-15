4f034496de5e262e5630b0bdfd739e748c3c0828
snowdong05 <28wdong@derryfield.org>
2025-01-15 11:02:25 -0500

    Update loops_copy_20250115105803.ino

--- loops_copy_20250115105803/loops_copy_20250115105803.ino
+++ loops_copy_20250115105803/loops_copy_20250115105803.ino
@@ -1,12 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
+  for (int i = 0; i <= 9; i++){
+    Serial.println(i);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i <= 9; i++){
-    Serial.println(i);
-  }
+  
+
 }
